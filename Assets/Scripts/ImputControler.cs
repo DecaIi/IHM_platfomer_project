2cfe10360da97366b4246f3477f9c73b0c1d97e0
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-10-23 15:11:18 +0200

    debug input controler

--- Assets/Scripts/ImputControler.cs
+++ Assets/Scripts/ImputControler.cs
@@ -17,37 +17,31 @@ public class ImputControler : MonoBehaviour
     float leftVertical;
     float padHorizontal;
     float padVertical;
-    bool dash;
+
     void Update()
     {
-        dash = false;
-        if (Input.GetButton("A")) //jum^p
-        {
-            playerControler.Jump(); 
-        }
-        if (Input.GetButton("B")) //dash
-        {
-            dash = true;
-        }
-        if (Input.GetButton("X"))
-        {                 
-                //nothing yet 
-        }
-        if (Input.GetButton("Y"))
-        {
-                //nothing yet 
-        }
 
         leftHorizontal = Input.GetAxis("LeftHorizontal");
         leftVertical = Input.GetAxis("LeftVertical");
         padHorizontal = Input.GetAxis("CrossHorizontal");
         padVertical = Input.GetAxis("CrossVertical");
-
         output  = new Vector2(Mathf.Clamp(leftHorizontal + padHorizontal, -1, 1) , Mathf.Clamp(leftVertical + padVertical, -1, 1) );
-        if (dash) //in case of dash
+      
+        if (Input.GetButton("A")) //jum^p
+        {
+            playerControler.Jump();
+        }
+        if (Input.GetButton("B")) //dash
         {
             playerControler.Dash(output);
-            return;
+        }
+        if (Input.GetButton("X"))
+        {
+            //nothing yet 
+        }
+        if (Input.GetButton("Y"))
+        {
+            //nothing yet 
         }
         playerControler.Move(output);
     }
