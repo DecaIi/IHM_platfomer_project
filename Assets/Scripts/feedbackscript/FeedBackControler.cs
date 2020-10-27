9b76bc3b1a6dff2c55587da0f7d5199eceaf8832
DecaIi <bastien.porte@telecom-sudparis.eu>
2020-10-27 10:07:26 +0100

    add bool to remove feedback

--- Assets/Scripts/feedbackscript/FeedBackControler.cs
+++ Assets/Scripts/feedbackscript/FeedBackControler.cs
@@ -14,7 +14,6 @@ public class FeedBackControler : MonoBehaviour
     Camerashake camerashake;
     SpriteRenderer renderer;
     
-  
     private void Start()
     {
         camerashake = playerCamera.GetComponent<Camerashake>();
@@ -26,40 +25,53 @@ public class FeedBackControler : MonoBehaviour
     }
     public void  CameraSharke()
     {
-        camerashake.Shake(shakeDuration,shakeIntensity);
+        if(true) { 
+            camerashake.Shake(shakeDuration,shakeIntensity);
+        }
     }
     /** instanciate the particule system on the given position
      *  param/ float position   position were we want the particul system to be instanciate  
      */
     public void InstanciateDashPrefabOnPosition(Vector3 position)
     {
-        ParticleSystem particleSystem = Instantiate(ParticleSystem,position,Quaternion.identity);
-        Destroy(particleSystem, particleSystem.main.duration); //destroy the particul system after his lif time 
+       if( true ) { 
+            ParticleSystem particleSystem = Instantiate(ParticleSystem,position,Quaternion.identity);
+            Destroy(particleSystem, particleSystem.main.duration); //destroy the particul system after his lif time 
+        }
     }
    /**change the color of the object for the given color 
     * param/ Color color        the color we want the object became 
     */
    public void ChangeColor(Color color)
     {
-        if (canchangecolor)
+        if (true)
         {
-            renderer.color = color;
+            if (canchangecolor)
+            {
+                renderer.color = color;
+            }
         }
     }
 
     bool canchangecolor = true;
     public void Clignote(Color color1, Color color2, float time)
     {
-        if (canchangecolor)
+        if (true)
         {
-            StartCoroutine(ClignoteEnumerator(color1, color2, time));
+            if (canchangecolor)
+            {
+                StartCoroutine(ClignoteEnumerator(color1, color2, time));
+            }
         }
     }
     public void Clignote(Color color1, Color color2)
     {
-        if (canchangecolor)
+        if (true)
         {
-            renderer.color = Color.Lerp(color1, color2, Mathf.PingPong(Time.time * 8, 1));
+            if (canchangecolor)
+            {
+                renderer.color = Color.Lerp(color1, color2, Mathf.PingPong(Time.time * 8, 1));
+            }
         }
     }
     IEnumerator ClignoteEnumerator(Color color1, Color color2, float time)
